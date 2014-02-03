ff6be20e436d2f10d32a6751bb8319a4182b28fb
SL-SamirRaorane <sraorane@shoplocal.com>
2014-02-03 10:01:03 -0600

    Click event enabled on the list

--- Shoppeezy/src/main/java/com/shoplocal/FullscreenActivity.java
+++ Shoppeezy/src/main/java/com/shoplocal/FullscreenActivity.java
@@ -1,7 +1,10 @@
 package com.shoplocal;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -27,13 +30,14 @@ import java.net.URL;
 
 public class FullscreenActivity extends Activity {
 
+    private ListView l;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_fullscreen);
 
-        ListView l = (ListView) findViewById(R.id.listview);
+        l = (ListView) findViewById(R.id.listview);
         /*String[] values = new String[] { "Pocket List", "Search Store", "Search Product",
                 "Trending" };*/
 
@@ -42,27 +46,21 @@ public class FullscreenActivity extends Activity {
                 android.R.layout.simple_list_item_1, values);
         l.setAdapter(adapter);
 
-        try {
-            getStores();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            public void onItemClick(AdapterView<?> arg0, View v, int position, long id)
+            {
 
-    private void getStores() throws IOException {
-        InputStream in = null;
-        URL url = null;
-        try {
-            url = new URL("http://vqascweb1.apimvc.crossmediaservices.com/retail/6883099d72e1ca52/2013.1/json/multiretailerpromotions?siteid=1506&MultRetPromoSort=1&pageimagewidth=189&citystatezip=60601");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            in = new BufferedInputStream(urlConnection.getInputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                AlertDialog.Builder adb;
+                adb = new AlertDialog.Builder(
+                        FullscreenActivity.this);
+                adb.setTitle("ListView OnClick");
+                adb.setMessage("Selected Item is = "
+                        + l.getItemAtPosition(position));
+                adb.setPositiveButton("Ok", null);
+                adb.show();
+            }
+        });
     }
 
     private String[] getData() {
