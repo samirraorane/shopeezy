e37cd9ee027011598bb9e84664ef78d786649ccd
SL-SamirRaorane <sraorane@shoplocal.com>
2014-02-03 16:45:12 -0600

    Images added to Listings view

--- Shoppeezy/src/main/java/com/shoplocal/ListingAdapter.java
+++ Shoppeezy/src/main/java/com/shoplocal/ListingAdapter.java
@@ -1,15 +1,22 @@
 package com.shoplocal;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.InputStream;
 
 public class ListingAdapter extends BaseAdapter {
     private Context mContext;
@@ -53,10 +60,12 @@ public class ListingAdapter extends BaseAdapter {
 
         String name = "";
         String price = "";
+        String imageUrl = "";
 
         try {
             name = listing.getString("Title");
             price = listing.getString("FinalPrice");
+            imageUrl = listing.getString("ImageLocation");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -73,6 +82,8 @@ public class ListingAdapter extends BaseAdapter {
         // load controls from layout resources
         TextView storeName = (TextView)v.findViewById(R.id.listingName);
         TextView storeDistance = (TextView)v.findViewById(R.id.listingPrice);
+        new DownloadImageTask((ImageView) v.findViewById(R.id.listingImage))
+                .execute(imageUrl);
 
         // set data to display
         storeName.setText(name);
@@ -80,5 +91,30 @@ public class ListingAdapter extends BaseAdapter {
 
         // return view
         return v;
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
